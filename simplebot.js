2eb76019785843d56b7ec701696b1f7adc4952cf
root <root@shatun.org>
2016-09-01 10:13:51 +0300

    add first simple bot

---
+++ simplebot.js
@@ -0,0 +1,41 @@
+//for CentOS:
+//yum install nodejs nmp
+//npm install node-telegram-bot-api
+//npm install request
+//
+//start - node <your app>
+//
+
+var TelegramBot = require('node-telegram-bot-api');
+ 
+var token = 'WRITE_YOUR_TOKEN_HERE';
+var botOptions = {
+    polling: true
+};
+var bot = new TelegramBot(token, botOptions);
+ 
+bot.getMe().then(function(me)
+{
+    console.log('Hello! My name is %s!', me.first_name);
+    console.log('My id is %s.', me.id);
+    console.log('And my username is @%s.', me.username);
+});
+ 
+bot.on('text', function(msg)
+{
+    var messageChatId = msg.chat.id;
+    var messageText = msg.text;
+    var messageDate = msg.date;
+    var messageUsr = msg.from.username;
+ 
+    if (messageText === '/say') {
+        sendMessageByBot(messageChatId, 'Hello World!');
+    }
+ 
+    console.log(msg);
+});
+ 
+function sendMessageByBot(aChatId, aMessage)
+{
+    bot.sendMessage(aChatId, aMessage, { caption: 'I\'m a cute bot!' });
+}
